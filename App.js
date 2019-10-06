3bec852cec32511a9bfe21449c296b8701ead10c
retuyu88 <andikaherup@gmail.com>
2019-10-06 08:08:14 +0700

    1. todo_list commit

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text,View,StyleSheet } from 'react-native';
+import { black } from 'ansi-colors';
+
+
+export default class App extends Component{
+
+  render() {
+    const todos = ['Work','Swim','Study','Sleep','Run'];
+
+    return (
+      <View style= {styles.container}>
+        
+        {todos.map((todo)=>{
+          return <Text style = {styles.text}>{todo}</Text>
+        })}
+      </View>
+    )
+  }
+};
+
+const styles = StyleSheet.create ({
+    container: {
+       padding: 10,
+       marginTop: 3,
+    },
+    text: {
+       color: '#4f603c',
+       borderBottomColor: 'black',
+       borderBottomWidth: 2,
+       paddingBottom: 5,
+       paddingTop:5
+    }
+ })
